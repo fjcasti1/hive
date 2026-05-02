41d5ae4f5e28a4050d2fe025ef4c30af2fe86bea
Kiko Castillo <fjcastillocarrasco@gmail.com>
2026-05-02 14:50:45 -0700

    Revert "wip (#15)"
    This reverts commit 11c1c9cb8bdda3c0657c11a4ad66273fa0216dd7.

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/fjcasti1/hive/cmd"
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var Version = "dev"
 
 func main() {
-	cmd.Execute()
+	cmd := &cobra.Command{
+		Use:     "hive",
+		Short:   "Manage multiple AI agent sessions in tmux",
+		Version: Version,
+	}
+	cmd.SetVersionTemplate("hive {{.Version}}\n")
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
